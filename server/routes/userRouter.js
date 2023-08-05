68345ce2873217a1fc5dfe047ab6043fa182f39a
lzwaaron <lzwaaron@gmail.com>
2023-08-05 14:20:56 -0400

    set up userController and userRouter

---
+++ server/routes/userRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const userRouter = express.Router();
+const userController = require('./userController');
+
+router.post(
+  '/signup',
+  userController.signup,
+  (req, res) => {
+    res.status(201).json({ message: 'User created!' });
+  },
+  (error, req, res, next) => {
+    res.status(500).json({ error: error.message || 'Signup failed!' });
+  }
+);
+
+router.post(
+  '/login',
+  userController.login,
+  (req, res) => {
+    res.status(200).json({ message: 'Logged in successfully!' });
+  },
+  (error, req, res, next) => {
+    res.status(500).json({ error: error.message || 'Login failed!' });
+  }
+);
+
+module.exports = userRouter;
