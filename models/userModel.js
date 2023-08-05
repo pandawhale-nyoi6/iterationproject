adc8f911ea0414d611687c8498596720091f42c7
Arjun <arjun@arjBook-Air.local>
2023-08-05 15:01:54 -0400

    removed unnecessary bcrypt code

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const SALT_WORK_FACTOR = 10;
-const bcrypt = require('bcryptjs');
-
 const locationSchema = new Schema({
     score: {type: Number},
     tags: [{type: String}]
