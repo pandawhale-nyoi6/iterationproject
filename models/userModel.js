565a8fbd4ca4ca94ae0431a1ef7f438bbaa40711
Arjun <arjun@arjBook-Air.local>
2023-08-05 14:34:41 -0400

    created user mongoDB schema

---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcryptjs');
+
+const locationSchema = new Schema({
+    score: {type: Number},
+    tags: [{type: String}]
+});
+
+const userSchema = new Schema({
+    firstName: {type: String},
+    lastName: {type: String},
+    beenList: [locationSchema],
+    savedList: [{type: String}],
+    friendList: [{type: String}],
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+});
+
+module.exports = mongoose.model('User', userSchema);
