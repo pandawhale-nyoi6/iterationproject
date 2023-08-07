1648320831180869796f9105dd48315a164d472d
amoomoo <uppalamulya@gmail.com>
2023-08-07 14:03:44 -0400

    add app.jsx back

---
+++ client/components/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes, // use Routes instead of Switch
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import LoginSignup from './LoginSignup.jsx';
+import UserPage from './UserPage.jsx';
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path='/login-signup'
+          element={
+            !isLoggedIn ? (
+              <LoginSignup onLogin={() => setIsLoggedIn(true)} setUser={(name) => setUsername(name)} />
+            ) : (
+              <Navigate to='/user' replace />
+            )
+          }
+        />
+        <Route
+          path='/user'
+          element={
+            isLoggedIn ? <UserPage username={username}/> : <Navigate to='/login-signup' replace />
+          }
+        />
+        <Route path='*' element={<Navigate to='/login-signup' replace />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
