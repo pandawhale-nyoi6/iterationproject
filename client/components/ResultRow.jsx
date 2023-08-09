6118efc2cfdf6cb589eccff4548861827e77ef9d
IsabelleRo <isabelle.ro@du.edu>
2023-08-09 19:34:10 -0400

    first query try

--- client/components/ResultRow.jsx
+++ client/components/ResultRow.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
+
 const ResultRow = (props) => {
     const { name, formatted_address, opening_hours, id } = props.result
-
+    const saveIt = (location,address) => {
+        console.log("in saveIt Function...!")
+        let dataBody = {
+            email: localStorage.email,
+            location: location,
+            address: address
+        };
+        fetch('/saveLoc', 
+        {method: 'POST',
+            body: dataBody,
+            headers: {
+            "Content-Type": "application/json",
+            },})
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+    }
     return (
         <tr>
             <td>{name}</td>
             <td>{formatted_address}</td>
             <button>Rate</button>
-            <button>Save</button>
+            <button onclick = {saveIt}>Save</button>
         </tr>
     );
 }
