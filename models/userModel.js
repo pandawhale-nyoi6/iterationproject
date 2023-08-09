cc9846181f466886392be450a99a77bc58a8635f
IsabelleRo <isabelle.ro@du.edu>
2023-08-09 14:01:37 -0400

    userModal

--- models/userModel.js
+++ models/userModel.js
@@ -28,9 +28,9 @@ const userSchema = new Schema({
   // added unique:true for email
   email: { type: String, required: true, unique: true },
   displayName: { type: String, required: true },
-  beenList: [locationSchema],
-  savedList: [locationSchema],
-  loveList: [locationSchema],
+  beenList: { type: Array, required: true },
+  savedList: { type: Array, required: true },
+  loveList: { type: Array, required: true },
   friendList: [{ type: String }],
 });
 
