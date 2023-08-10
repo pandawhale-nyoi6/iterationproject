a1be8a93ff4391bd557f4e6dc23acb67b9c0f87f
Julia Shi <jshi@Julias-MacBook-Pro-2.local>
2023-08-10 10:24:20 -0400

    added styling with tailwind and daisyUI to the LoginSignup and UserPage

--- client/components/LoginSignup.jsx
+++ client/components/LoginSignup.jsx
@@ -22,13 +22,34 @@ const LoginSignup = ({ onLogin, setUser }) => {
   };
 
   return (
-
-    <div className='signup-container'>
-      <button className="font-primary bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={(e) => handleGoogleSignIn(e)}>GOOGLE AUTH!!!!!!</button>
+<div
+  className="relative overflow-hidden rounded-lg bg-cover bg-no-repeat p-12 text-center object-fill"
+  style={{'backgroundImage': `url('https://img.freepik.com/free-vector/gradient-abstract-blurred-grainy-background_1188-710.jpg')`, height: '100%'}}>
+  <div
+    className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+    style={{'backgroundColor': 'rgba(0, 0, 0, 0.1)'}}>
+    <div className="flex h-full items-center justify-center">
+      <div className="text-white">
+        <h1 className="font-primary text-8xl text-white" style={{'textShadow': '5px 5px 20px black'}}>VIBE*</h1>
+        <h4 className="mb-6 text-xl font-semibold" style={{'textShadow': '5px 5px 20px black'}}>The world's most popular meeting app</h4>
+        <button className="font-primary bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={(e) => handleGoogleSignIn(e)}>
+        <p>
+          <img className="pr-4" src="https://img.freepik.com/free-icon/search_318-265146.jpg"  style={{height: '25px', 'display':'inline-block'}}></img>
+             Sign in with Google
+        </p>
+          </button>
+      </div>
     </div>
+  </div>
+</div>
 
 
   );
 };
 
+    // <div className='signup-container'>
+    //   <button className="font-primary bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={(e) => handleGoogleSignIn(e)}>GOOGLE AUTH!!!!!!</button>
+    // </div>
+
+    //<h2 className="mb-4 text-4xl font-semibold">Heading</h2>
 export default LoginSignup;
--- client/components/UserPage.jsx
+++ client/components/UserPage.jsx
@@ -4,7 +4,11 @@ import { useNavigate } from 'react-router-dom'; // import useNavigate
 
 const UserPage = ({ userEmail }) => {
   const navigate = useNavigate(); // Use the useNavigate hook
-  const [savedList, setSavedList] = useState([]);
+  const [savedList, setSavedList] = useState([
+    {name: 'Capital One Cafe', address: '123 abc st'}, 
+    {name: 'Bean & Bean Chelsea', address: '123 abc st'}, 
+    {name: 'Gregorys Coffee', address: '123 abc st'}, 
+  ]);
   const [triedList, setTriedList] = useState([]);
   const getSaved = async () => {
     try {
@@ -42,7 +46,8 @@ const UserPage = ({ userEmail }) => {
   const savedRows = savedList.map((savedPlace) => {
     return (
       <tr>
-        <td>{savedPlace}</td>
+        <td>{savedPlace.name}</td>
+        <td>{savedPlace.address}</td>
       </tr>
     );
   });
@@ -60,18 +65,93 @@ const UserPage = ({ userEmail }) => {
     );
   });
 
+
   return (
-    <div>
+    <div className="bg-gradient-to-r from-blue-200"  style={{height: '100%'}}> 
+      <h1 className="font-primary text-8xl text-primary" style={{'textShadow': '2px 2px 10px gray'}}>VIBE*</h1>
       {/* add a button to navigate to the search page */}
-      <div className='lists'>
-        <table className='SavedTable'>
-          <thead>
-            <tr>
-              <h4>Saved Spots!</h4>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
+      <div className='lists overflow-x-auto flex-col'>
+      <br />
+      <br />
+        <div className="collapse show bg-base-200" style={{'backgroundColor': 'rgba(0, 0, 0, 0.2)'}}>
+          <input type="checkbox" /> 
+          <div className="collapse-title text-2xl font-medium font-primary text-white" style={{'textShadow': '1px 1px 2px black'}}>
+             Saved Places!
+          </div>
+        <div className="collapse-content"> 
+        <table className='SavedTable table table-zebra font-primary rounded-sm'  style={{'backgroundColor': 'rgba(0, 0, 0, 0.1)'}}>
+            <thead>
+              <tr>
+                <th>Place</th>
+                <th>Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              {savedRows}
+            </tbody>
+          </table>
+        </div>
+        </div>
+        <br />
+        <div className="collapse show bg-base-200" style={{'backgroundColor': 'rgba(0, 0, 0, 0.2)'}}>
+          <input type="checkbox" /> 
+          <div className="collapse-title text-2xl font-medium font-primary text-white" style={{'textShadow': '1px 1px 2px black'}}>
+          **~ Where you've been ~**
+          </div>
+        <div className="collapse-content"> 
+        <table className='SavedTable table table-zebra font-primary rounded-sm'  style={{'backgroundColor': 'rgba(0, 0, 0, 0.1)'}}>
+        <thead>
+              <tr>
+                <th><h4>Place</h4></th>
+                <th><h4>Rating</h4></th>
+                <th><h4>Tags</h4></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Seven Grams Caffe</th>
+                <th>4</th>
+                <th>Chill</th>
+              </tr>
+              <tr>
+                <th>Intelligentsia Coffee Highline Coffeebar</th>
+                <th>4</th>
+                <th>Quiet</th>
+              </tr>
+              <tr>
+                <th>Stumptown Coffee Roasters</th>
+                <th>3</th>
+                <th>Chill</th>
+              </tr>
+              <tr>
+                <th>Gregorys Coffee</th>
+                <th>4</th>
+                <th>Outlets</th>
+              </tr>
+              <tr>
+                <th>Joe Coffee Company</th>
+                <th>2</th>
+                <th>Clean Bathroom</th>
+              </tr>
+            </tbody>
+        </table>
+        </div>
+        </div>
+        <br />
+        <br />
+      </div>
+      <div className='searchButton'>
+        <button className='button' onClick={() => navigate('/search')}>Go to Search Page</button>
+      </div>
+    </div>
+  );
+};
+export default UserPage;
+
+
+
+
+{/* <tr>
               <th>Capital One Cafe</th>
             </tr>
             <tr>
@@ -85,50 +165,48 @@ const UserPage = ({ userEmail }) => {
             </tr>
             <tr>
               <th>King's Street Coffee</th>
-            </tr>
-          </tbody>
-        </table>
-        <table className='BeenTable'>
-          <thead>
-            <tr>
-              <th><h4>Where You've Been</h4></th>
-              <th><h4>Rating</h4></th>
-              <th><h4>Tags</h4></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>Seven Grams Caffe</th>
-              <th>4</th>
-              <th>Chill</th>
-            </tr>
-            <tr>
-              <th>Intelligentsia Coffee Highline Coffeebar</th>
-              <th>4</th>
-              <th>Quiet</th>
-            </tr>
-            <tr>
-              <th>Stumptown Coffee Roasters</th>
-              <th>3</th>
-              <th>Chill</th>
-            </tr>
-            <tr>
-              <th>Gregorys Coffee</th>
-              <th>4</th>
-              <th>Outlets</th>
-            </tr>
-            <tr>
-              <th>Joe Coffee Company</th>
-              <th>2</th>
-              <th>Clean Bathroom</th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className='searchButton'>
-        <button className='button' onClick={() => navigate('/search')}>Go to Search Page</button>
-      </div>
-    </div>
-  );
-};
-export default UserPage;
+            </tr> */}
+
+
+
+
+            // <h2 className="font-primary text-2xl text-primary" style={{'textShadow': '1px 1px 2px gray'}}>**~ Where you've been ~**</h2>
+            // <div>
+            //   <table className='SavedTable table table-zebra font-primary'  style={{'backgroundColor': 'rgba(0, 0, 0, 0.1)'}}>
+            //     <thead>
+            //       <tr>
+            //         <th><h4>Place</h4></th>
+            //         <th><h4>Rating</h4></th>
+            //         <th><h4>Tags</h4></th>
+            //       </tr>
+            //     </thead>
+            //     <tbody>
+            //       <tr>
+            //         <th>Seven Grams Caffe</th>
+            //         <th>4</th>
+            //         <th>Chill</th>
+            //       </tr>
+            //       <tr>
+            //         <th>Intelligentsia Coffee Highline Coffeebar</th>
+            //         <th>4</th>
+            //         <th>Quiet</th>
+            //       </tr>
+            //       <tr>
+            //         <th>Stumptown Coffee Roasters</th>
+            //         <th>3</th>
+            //         <th>Chill</th>
+            //       </tr>
+            //       <tr>
+            //         <th>Gregorys Coffee</th>
+            //         <th>4</th>
+            //         <th>Outlets</th>
+            //       </tr>
+            //       <tr>
+            //         <th>Joe Coffee Company</th>
+            //         <th>2</th>
+            //         <th>Clean Bathroom</th>
+            //       </tr>
+            //     </tbody>
+            //   </table>
+    
+            // </div>
